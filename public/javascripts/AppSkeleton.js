7faa8f69e4a5dae0cad06785f0bc5420b4f20253
liukan <liukan@bytedance.com>
2017-12-16 00:58:02 +0800

    add terrian
    Change-Id: Ibde9ce0039435ff860ab40c3d9a22ebbce832f83

--- public/javascripts/AppSkeleton.js
+++ public/javascripts/AppSkeleton.js
@@ -43,13 +43,13 @@
     //////////////////////////////////////////////////////////////////////////
 
     // // Load STK World Terrain
-    // viewer.terrainProvider = new Cesium.CesiumTerrainProvider({
-    //     url : 'https://assets.agi.com/stk-terrain/world',
-    //     requestWaterMask : true, // required for water effects
-    //     requestVertexNormals : false // required for terrain lighting
-    // });
+    viewer.terrainProvider = new Cesium.CesiumTerrainProvider({
+        url : 'https://assets.agi.com/stk-terrain/world',
+        requestWaterMask : true, // required for water effects
+        requestVertexNormals : false // required for terrain lighting
+    });
     // // Enable depth testing so things behind the terrain disappear.
-    // viewer.scene.globe.depthTestAgainstTerrain = true;
+    viewer.scene.globe.depthTestAgainstTerrain = true;
 
     //////////////////////////////////////////////////////////////////////////
     // Configuring the Scene
