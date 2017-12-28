2a5f82cca5b5f56f7e96d0889bc99c82bd1ce1e1
liukan <liukan@bytedance.com>
2017-12-28 13:52:49 +0800

    upload size < 2m
    Change-Id: I0896a73e14ff25f21fa2b4ce5d64abf1981261f5

--- public/javascripts/avatar.js
+++ public/javascripts/avatar.js
@@ -222,6 +222,10 @@
     const grid_link = $("#set-grid-link").val()
     formData.append('grid_link', grid_link)
 
+    xhr.onerror = (err) => {
+      showError("upload Error:" + err);
+    }
+
     $validation.signWithTimestamp(web3, function(err, signature) {
       if (err) {
         console.error('Error occured when sign with timestamp');
